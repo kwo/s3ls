ec9fd190790ed8094653e017f9e8a585b1ec06a8
Karl Ostendorf <karl@ostendorf.com>
2020-04-25 17:06:08 +0200

    print as json
    Signed-off-by: Karl Ostendorf <karl@ostendorf.com>

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 	"os/signal"
 	"runtime"
 	"sync"
 	"syscall"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
@@ -29,8 +32,47 @@ type Config struct {
 }
 
 type Item struct {
-	Key         string
-	ContentType string
+	Object   s3.Object
+	Metadata *s3.HeadObjectOutput
+}
+
+type Entry struct {
+	Key                       string            `json:"key"`
+	Invalid                   bool              `json:"invalid,omitempty"`
+	Size                      int64             `json:"size"`
+	OwnerID                   string            `json:"ownerId,omitempty"`
+	OwnerName                 string            `json:"ownerName,omitempty"`
+	ETagObject                string            `json:"etagObject,omitempty"`
+	LastModifiedObject        *time.Time        `json:"lastModifiedObject,omitempty"`
+	StorageClassObject        string            `json:"storageClassObject,omitempty"`
+	AcceptRanges              string            `json:"acceptRanges,omitempty"`
+	CacheControl              string            `json:"cacheControl,omitempty"`
+	ContentDisposition        string            `json:"contentDisposition,omitempty"`
+	ContentEncoding           string            `json:"contentEncoding,omitempty"`
+	ContentLanguage           string            `json:"contentLanguage,omitempty"`
+	ContentLength             int64             `json:"contentLength,omitempty"`
+	ContentType               string            `json:"contentType,omitempty"`
+	DeleteMarker              bool              `json:"deleteMarker,omitempty"`
+	ETag                      string            `json:"etag,omitempty"`
+	Expiration                string            `json:"expiration,omitempty"`
+	Expires                   string            `json:"expires,omitempty"`
+	LastModified              *time.Time        `json:"lastModified,omitempty"`
+	Metadata                  map[string]string `json:"metadata,omitempty"`
+	MissingMeta               int64             `json:"missingMeta,omitempty"`
+	ObjectLockLegalHoldStatus string            `json:"objectLockLegalHoldStatus,omitempty"`
+	ObjectLockMode            string            `json:"objectLockMode,omitempty"`
+	ObjectLockRetainUntilDate *time.Time        `json:"objectLockRetainUntilDate,omitempty"`
+	PartsCount                int64             `json:"partsCount,omitempty"`
+	ReplicationStatus         string            `json:"replicationStatus,omitempty"`
+	RequestCharged            string            `json:"requestCharged,omitempty"`
+	Restore                   string            `json:"restore,omitempty"`
+	SSECustomerAlgorithm      string            `json:"sseCustomerAlgorithm,omitempty"`
+	SSECustomerKeyMD5         string            `json:"sseCustomerKeyMD5,omitempty"`
+	SSEKMSKeyId               string            `json:"sseKMSKeyId,omitempty"`
+	ServerSideEncryption      string            `json:"serverSideEncryption,omitempty"`
+	StorageClass              string            `json:"storageClass,omitempty"`
+	VersionId                 string            `json:"versionId,omitempty"`
+	WebsiteRedirectLocation   string            `json:"websiteRedirectLocation,omitempty"`
 }
 
 func main() {
@@ -48,7 +90,6 @@ func main() {
 	s3Session := s3.New(awsSession, &aws.Config{})
 
 	ctx, killSwitch := exitContext()
-
 	listBucketContents(ctx, killSwitch, s3Session, cfg.Bucket, cfg.Workers)
 
 }
@@ -63,9 +104,8 @@ func listBucketContents(ctx context.Context, killSwitch func(error), s3Session *
 	}
 	items := merge(workers...)
 
-	for item := range items {
-		fmt.Printf("%s %s\n", item.Key, item.ContentType)
-	}
+	entries := toEntries(items)
+	printEntriesAsJson(os.Stdout, entries)
 
 }
 
@@ -103,8 +143,8 @@ func fetchMetadata(ctx context.Context, killSwitch func(error), s3Session *s3.S3
 				return
 			}
 			out <- Item{
-				Key:         *object.Key,
-				ContentType: *head.ContentType,
+				Object:   object,
+				Metadata: head,
 			}
 		}
 		close(out)
@@ -139,6 +179,77 @@ func merge(workers ...<-chan Item) <-chan Item {
 
 	return out
 
+}
+
+func toEntries(items <-chan Item) <-chan Entry {
+
+	out := make(chan Entry)
+
+	go func() {
+		for item := range items {
+			entry := Entry{
+				Key:                aws.StringValue(item.Object.Key),
+				Size:               aws.Int64Value(item.Object.Size),
+				ETagObject:         aws.StringValue(item.Object.ETag),
+				LastModifiedObject: item.Object.LastModified,
+				StorageClassObject: aws.StringValue(item.Object.StorageClass),
+			}
+			if item.Object.Owner != nil {
+				entry.OwnerID = aws.StringValue(item.Object.Owner.ID)
+				entry.OwnerName = aws.StringValue(item.Object.Owner.DisplayName)
+			}
+			entry.Invalid = item.Metadata == nil
+			if !entry.Invalid {
+				entry.AcceptRanges = aws.StringValue(item.Metadata.AcceptRanges)
+				entry.CacheControl = aws.StringValue(item.Metadata.CacheControl)
+				entry.ContentDisposition = aws.StringValue(item.Metadata.ContentDisposition)
+				entry.ContentEncoding = aws.StringValue(item.Metadata.ContentEncoding)
+				entry.ContentLanguage = aws.StringValue(item.Metadata.ContentLanguage)
+				entry.ContentLength = aws.Int64Value(item.Metadata.ContentLength)
+				entry.ContentType = aws.StringValue(item.Metadata.ContentType)
+				entry.DeleteMarker = aws.BoolValue(item.Metadata.DeleteMarker)
+				entry.ETag = aws.StringValue(item.Metadata.ETag)
+				entry.Expiration = aws.StringValue(item.Metadata.Expiration)
+				entry.Expires = aws.StringValue(item.Metadata.Expires)
+				entry.LastModified = item.Metadata.LastModified
+				entry.Metadata = aws.StringValueMap(item.Metadata.Metadata)
+				entry.MissingMeta = aws.Int64Value(item.Metadata.MissingMeta)
+				entry.ObjectLockLegalHoldStatus = aws.StringValue(item.Metadata.ObjectLockLegalHoldStatus)
+				entry.ObjectLockMode = aws.StringValue(item.Metadata.ObjectLockMode)
+				entry.ObjectLockRetainUntilDate = item.Metadata.ObjectLockRetainUntilDate
+				entry.PartsCount = aws.Int64Value(item.Metadata.PartsCount)
+				entry.ReplicationStatus = aws.StringValue(item.Metadata.ReplicationStatus)
+				entry.RequestCharged = aws.StringValue(item.Metadata.RequestCharged)
+				entry.Restore = aws.StringValue(item.Metadata.Restore)
+				entry.SSECustomerAlgorithm = aws.StringValue(item.Metadata.SSECustomerAlgorithm)
+				entry.SSECustomerKeyMD5 = aws.StringValue(item.Metadata.SSECustomerKeyMD5)
+				entry.SSEKMSKeyId = aws.StringValue(item.Metadata.SSEKMSKeyId)
+				entry.ServerSideEncryption = aws.StringValue(item.Metadata.ServerSideEncryption)
+				entry.StorageClass = aws.StringValue(item.Metadata.StorageClass)
+				entry.VersionId = aws.StringValue(item.Metadata.VersionId)
+				entry.WebsiteRedirectLocation = aws.StringValue(item.Metadata.WebsiteRedirectLocation)
+			}
+			out <- entry
+		}
+		close(out)
+	}()
+
+	return out
+
+}
+
+func printEntriesAsJson(w io.Writer, entries <-chan Entry) {
+	notRowOne := false
+	_, _ = w.Write([]byte("["))
+	for entry := range entries {
+		if notRowOne {
+			_, _ = w.Write([]byte(","))
+		}
+		data, _ := json.Marshal(entry)
+		_, _ = w.Write(data)
+		notRowOne = true
+	}
+	_, _ = w.Write([]byte("]"))
 }
 
 func exitContext() (context.Context, func(error)) {
